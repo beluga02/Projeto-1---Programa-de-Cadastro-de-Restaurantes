03cce94f6191ae0f00956fd710a552d4dbe202f0
Letícia Matias <66281122+beluga02@users.noreply.github.com>
2021-09-20 13:40:54 -0300

    Create Tipo.cs

---
+++ Enum/Tipo.cs
@@ -0,0 +1,29 @@
+namespace Projeto1
+{
+    public enum Tipo
+    {
+        Italiano = 1,
+
+        Brasileiro = 2,
+
+        Frances = 3,
+
+        Arabe = 4,
+
+        Indiano = 5, 
+
+        Peruano = 6, 
+
+        Argentino = 7,
+
+        Mexicano = 8,
+
+        Hungaro = 9,
+
+        Japones = 10,
+
+        Tailandes = 11
+
+
+    }
+}
